09b30f77c46ac6821dc1d54e4a762181ba40c181
kazutosinisikawa <yappaniside627@yahoo.co.jp>
2020-08-30 21:37:56 +0900

    lesson7 課題完了まで
    ブロックと地面の衝突時に音が鳴るようにした

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,6 +10,7 @@ public class CubeController : MonoBehaviour
     // 消滅位置
     private float deadLine = -10;
 
+
     // Use this for initialization
     void Start()
     {
@@ -25,6 +26,16 @@ public class CubeController : MonoBehaviour
         if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //キューブ同士、もしくは地面に当たった場合、音を鳴らす
+        if (collision.collider.tag == "GroundTag" || collision.collider.tag == "CubeTag")
+        {
+            GetComponent<AudioSource>().Play();  // 効果音を鳴らす
+            Debug.Log("音が鳴る予定");
         }
     }
 }
